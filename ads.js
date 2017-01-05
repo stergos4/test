73d5067cea612a22543bd4bde208463cf095b281
stergos4 <john_kental@yahoo.com>
2017-01-05 13:16:08 +0200

    Delete ads.js

--- ads.js
+++
@@ -1,8 +0,0 @@
-var e=document.createElement('div'); e.id='qpezXfbWomRi'; e.style.display='none'; document.body.appendChild(e);
-
-var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-
-if(!is_chrome)
-{
-e=document.createElement('div'); e.id='qpezXfbWomRi'; e.style.display='block'; document.body.appendChild(e);
-}  
