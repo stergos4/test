62353bed8a5ef7bd6b5f085280be6755f5b89685
stergos4 <john_kental@yahoo.com>
2017-01-05 13:07:53 +0200

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,8 @@
+var e=document.createElement('div'); e.id='qpezXfbWomRi'; e.style.display='none'; document.body.appendChild(e);
+
+var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+
+if(!is_chrome)
+{
+e=document.createElement('div'); e.id='qpezXfbWomRi'; e.style.display='block'; document.body.appendChild(e);
+}  
